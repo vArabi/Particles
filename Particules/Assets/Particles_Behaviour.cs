070b4854e88b40be0381d777e557d75325b56d86
vArabi <victor.arabi@gmail.com>
2019-10-08 00:09:42 +0200

    Initial commit

---
+++ Particules/Assets/Particles_Behaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles_Behaviour : MonoBehaviour
+{
+
+    public GameObject FireParticles;
+    public GameObject RainParticles;
+
+    public bool Fire = true;
+    public bool Rain = false;
+
+
+
+	void Start ()
+    {
+        FireParticles.SetActive(Fire);
+        RainParticles.SetActive(Rain);
+    }
+	
+
+
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(waiter());
+        }
+        FireParticles.SetActive(Fire);
+        RainParticles.SetActive(Rain);
+    }
+
+    IEnumerator waiter()
+    {
+        Rain = !Rain;
+        yield return new WaitForSeconds(1);
+        Fire = !Fire;
+    }
+}
